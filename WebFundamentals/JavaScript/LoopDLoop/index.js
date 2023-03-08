93cf69479bbef2600081fb984c8b5a03b505bf8c
Jake Dutra <hdcxrti@gmail.com>
2023-03-07 21:36:17 -0500

    LoopDLoop

---
+++ WebFundamentals/JavaScript/LoopDLoop/index.js
@@ -0,0 +1,15 @@
+for (let runner = 0; runner < 7; runner++) {
+  if (runner < 2) {
+    console.log("No Candy", runner);
+  } else if (runner == 2) {
+    console.log("Yay Candy");
+  } else if (runner < 4) {
+    console.log("No Candy", runner);
+  } else if (runner == 4) {
+    console.log("Yay Candy");
+  } else if (runner < 6) {
+    console.log("No Candy", runner);
+  } else if (runner == 6) {
+    console.log("Yay Candy");
+  }
+}
